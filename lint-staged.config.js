0bea667927f50968b12fc5b74144275336fcfbf3
Shayne Preston <shayne.preston@protonmail.com>
2025-11-26 09:22:58 -0500

    use prettier config everywhere

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
-  '**/*.{css,html,js,jsx,json,less,md,scss,ts,tsx,yaml}': ['prettier --write'],
+  '**/*.{css,html,js,jsx,json,less,md,scss,ts,tsx,yaml}': ['prettier --config ./prettier.config.ts --write'],
   'src/**/*.{js,jsx,ts,tsx}': ['eslint --max-warnings=0 --fix'],
   '**/*.{js,ts}?(x)': () => 'tsc -p tsconfig.json --noEmit',
 };
